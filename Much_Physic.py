224dd8ca89e7772634055fdb677937286397c787
That Chair <68354875+XD5000@users.noreply.github.com>
2023-05-29 20:22:37 -0700

    Updated to 0.0.2a

--- Much_Physic.py
+++ Much_Physic.py
@@ -1,7 +1,9 @@
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame as pg
 import numpy as np
 
-G = 6.67
+G = 0.00667
 
 pg.init()
 
@@ -47,39 +49,27 @@ class Particle:
         self.pos = pg.Vector2(convert_coords(pg.mouse.get_pos()))
         self.velocity = pg.Vector2(0, 0)
         self.colour = (255,255,255)
+        self.acceleration = pg.Vector2(0, 0)
     def update(self, particles):
-        self.pos.update(self.pos + self.velocity)
+        self.pos.update(self.pos + (self.velocity * 30 / fps))
 
-        if self.pos.y - self.radius < 0:
-            self.pos.y = self.radius
-            self.velocity.y *= -self.elasticity
-        elif self.pos.y + self.radius < res[1]:
-            self.velocity.y -= univ_gravity
-        else:
-            self.pos.y = res[1] - self.radius
-            self.velocity.y *= -self.elasticity
+        colliding_particle = False
 
-        if self.pos.x - self.radius < 250:
-            self.pos.x = 250 + self.radius
-            self.velocity.x *= -self.elasticity
-        elif self.pos.x + self.radius > res[0]:
-            self.pos.x = res[0] - self.radius
-            self.velocity.x *= -self.elasticity
-
-        for particle in particles:
+        for particle in particles: 
             if particle.id != self.id:
                 between = pg.Vector2(self.pos.x - particle.pos.x, self.pos.y - particle.pos.y)
                 opp_between = pg.Vector2(particle.pos.x - self.pos.x, particle.pos.y - self.pos.y)
                 # collision
                 total_mass = self.mass + particle.mass
                 if between.magnitude() <= self.radius + particle.radius:
+                    colliding_particle = True
                     # print("Collision")
                     if between.magnitude() < self.radius + particle.radius and between.magnitude() != 0:  
                         try:
                             between.scale_to_length((self.radius + particle.radius + 1))
                         except:
                             between.update(1, 1)
-                            between.scale_to_length((self.radius + particle.radius + 1))
+                            between.scale_to_length((self.radius + particle.radius + 1) / 2)
                             opp_between.update(-1, -1)
                         self.pos.x = particle.pos.x + between.x
                         self.pos.y = particle.pos.y + between.y
@@ -100,12 +90,28 @@ class Particle:
 
                 opp_between = pg.Vector2(particle.pos.x - self.pos.x, particle.pos.y - self.pos.y)
                 if opp_between.magnitude() != 0:
-                    force = G*(particle.mass / (opp_between.magnitude() ** 2))
+                    force = G*(particle.mass / (opp_between.magnitude() ** 2)) * 30/fps
                     opp_between.scale_to_length(force)
                     self.velocity.update(self.velocity + opp_between)
 
             if particle.id == self.connected_particle:
                 self.connected_particle_pos = particle.pos
+        if self.pos.y - self.radius < 0:
+            self.pos.y = self.radius
+            self.velocity.y *= -self.elasticity
+        if self.pos.y + self.radius < res[1] and not colliding_particle:
+            self.velocity.y -= univ_gravity * 30/fps
+        elif self.pos.y + self.radius > res[1]:
+            self.pos.y = res[1] - self.radius
+            self.velocity.y *= -self.elasticity
+            self.velocity.y -= univ_gravity
+
+        if self.pos.x - self.radius < 250:
+            self.pos.x = 250 + self.radius
+            self.velocity.x *= -self.elasticity
+        elif self.pos.x + self.radius > res[0]:
+            self.pos.x = res[0] - self.radius
+            self.velocity.x *= -self.elasticity
                                                 
 
     def show(self, surface: pg.Surface):
@@ -126,7 +132,7 @@ class Slider():
 res = (913, 500)
 sim = pg.display.set_mode(res, pg.RESIZABLE + pg.SCALED)
 
-pg.display.set_caption("Much Physic Simulation")
+pg.display.set_caption("Much Physic: Particle Simulation")
 
 particles = np.array([])
 
@@ -145,7 +151,7 @@ settings = font.render("SETTINGS", True, (255, 255, 255))
 settings_rect = settings.get_rect(center=(115, 15))
 
 particle_size = 20
-size = smol_font.render("Particle Size", True, (255, 255, 255))
+size = smol_font.render("Particle Size (Px)", True, (255, 255, 255))
 size_rect = size.get_rect(center=(115, 40))
 size_slider = pg.Rect(5, 55, 200, 16)
 size_slider_pos = ((particle_size * 4) + 5, 63)
@@ -156,12 +162,12 @@ particle_weight = 100
 weight = smol_font.render("Particle Mass", True, (255, 255, 255))
 weight_rect = weight.get_rect(center=(115, 90))
 weight_slider = pg.Rect(5, 105, 200, 16)
-weight_slider_pos = ((particle_weight / 5) + 5, 113)
+weight_slider_pos = ((particle_weight / 4) + 5, 113)
 weight_sliding = False
 weight_text_pos = (220, 105)
 
 particle_elasticity = 0.95
-elas = smol_font.render("Particle Elasticity", True, (255, 255, 255))
+elas = smol_font.render("Particle Elasticity (%)", True, (255, 255, 255))
 elas_rect = elas.get_rect(center=(115, 140))
 elas_slider = pg.Rect(5, 155, 200, 16)
 elas_slider_pos = ((particle_elasticity * 200) + 5, 163)
@@ -171,7 +177,7 @@ elas_text_pos = (220, 155)
 
 univ_gravity_scale = 1
 univ_gravity = univ_gravity_scale * 0.49
-univ = smol_font.render("Universal Gravity", True, (255, 255, 255))
+univ = smol_font.render("Universal Gravity Multiplier", True, (255, 255, 255))
 univ_rect = univ.get_rect(center=(115, 190))
 univ_slider = pg.Rect(5, 205, 200, 16)
 univ_slider_pos = ((univ_gravity_scale * 96) + 13, 213)
@@ -181,6 +187,7 @@ univ_text_pos = (220, 205)
 num_particles = 0
 
 while running:
+    fps = clock.get_fps()
     mouse_pos = pg.mouse.get_pos()
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -189,20 +196,20 @@ while running:
             if event.key == pg.K_r:
                 text = "Simulation Reset"
                 num_particles = 0
-                text_time_left = 30
+                text_time_left = fps
                 particles = np.array([])
             if event.key == pg.K_p or event.key == pg.K_SPACE:
                 paused = not paused
                 if paused:
                     text = "Simulation Paused"
-                    text_time_left = 30
+                    text_time_left = fps
                 else:
                     text = "Simulation Resumed"
-                    text_time_left = 30
+                    text_time_left = fps
             if paused:
                 if event.key == pg.K_PERIOD:
-                    text = "Simulation Advanced 1 Frame"
-                    text_time_left = 30
+                    text = "Simulation Advanced"
+                    text_time_left = fps
                     for particle in particles:
                         particle.update(particles)
         elif event.type == pg.MOUSEBUTTONDOWN:
@@ -231,7 +238,7 @@ while running:
                 particle.velocity.update(mouse_change)
                 particles = np.append(particles, particle)
                 text = "Particle Spawned"
-                text_time_left = 30
+                text_time_left = fps
             size_sliding = False
             weight_sliding = False
             elas_sliding = False
@@ -249,10 +256,10 @@ while running:
     if mouse_down:
         if size_sliding:
             size_slider_pos = (coerce_in(mouse_pos[0], (13, 205)), 63)
-            particle_size = (size_slider_pos[0] - 5) / 4
+            particle_size = round(size_slider_pos[0] + 39) / 8
         elif weight_sliding:
             weight_slider_pos = (coerce_in(mouse_pos[0], (13, 205)), 113)
-            particle_weight = (weight_slider_pos[0] - 5) * 5
+            particle_weight = (weight_slider_pos[0] - 5) * 4
         elif elas_sliding:
             elas_slider_pos = (coerce_in(mouse_pos[0], (13, 205)), 163)
             particle_elasticity = (elas_slider_pos[0] - 5) / 200
@@ -281,9 +288,12 @@ while running:
     pg.draw.rect(sim, (75, 75, 75), univ_slider, border_radius= 7)
     pg.draw.circle(sim, (255, 255, 255), univ_slider_pos, 12)
 
+    
+
     if text_time_left > 0:
         text_time_left -= 1
-        sim.blit(font.render(text, True, ((255 - 50) * (text_time_left / 30) + 50, (255 - 50) * (text_time_left / 30) + 50, (255 - 50) * (text_time_left / 30) + 50)), (10, res[1] - 35))
+        text_time = coerce_in(text_time_left / fps, (0, 1))
+        sim.blit(font.render(text, True, ((255 - 50) * text_time + 50, (255 - 50) * text_time+ 50, (255 - 50) * text_time + 50)), (10, res[1] - 35))
 
     sim.blit(settings, settings_rect)
 
@@ -299,8 +309,9 @@ while running:
     sim.blit(univ, univ_rect)
     sim.blit(smol_font.render(str(round(univ_gravity_scale, 2)), True, (255, 255, 255)), univ_text_pos)
 
+    sim.blit(smol_font.render(f"FPS: {round(fps)}", True, (255, 255, 255)), (0,0))
     pg.display.flip()
     # --- Limit to 60 frames per second
-    clock.tick(30)
+    clock.tick_busy_loop()
     # print()
 pg.quit()
